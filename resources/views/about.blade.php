7d9de84599274a54342c13edc175c84c369d64ff
Dedo_Finger2 <antonioimportant@gmail.com>
2023-08-03 16:35:38 -0300

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>Sbore da aplicação</h1>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'Contact')
+
+@section('content')
+    <h1>Contact da aplicação</h1>
+@endsection
